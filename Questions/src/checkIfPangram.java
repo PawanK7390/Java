e43128d476ff5e4a4d67ad08f2cddce1ae1b6b10
Pawan Kapoor <kapoorpawan7390@gmail.com>
2024-08-04 21:32:04 +0530

    1 question

--- Questions/src/checkIfPangram.java
+++ Questions/src/checkIfPangram.java
@@ -2,19 +2,23 @@ import java.util.HashSet;
 import java.util.Set;
 public class checkIfPangram {
     public boolean checkIfPangram(String sentence) {
-        // Create a Set to store unique letters
-        Set<Character> uniqueLetters = new HashSet<>();
+        Set<Character> alphabetSet = new HashSet<>();
 
-        // Iterate through each character in the sentence
-        for (char c : sentence.toCharArray()) {
-            // Add the character to the Set if it's a letter
-            if (Character.isLetter(c)) {
-                uniqueLetters.add(c);
-            }
+        // Add all the characters from a to z in hashset
+        for(int i = 'a'; i <= 'z'; i++){
+            alphabetSet.add((char) i);
         }
 
-        // Check if the Set contains 26 unique letters
-        return uniqueLetters.size() == 26;
+        // remove every character and check
+        // if set becomes empty at any point of time
+        for(int i = 0; i < sentence.length(); i++){
+            alphabetSet.remove(sentence.charAt(i));
+
+            if(alphabetSet.isEmpty()){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
